6243bfda051715e0088f68949d2e78c110bbc3f5
Chance Eakin <chance.eakin@gmail.com>
2018-12-24 09:40:21 -0600

    nav dropdown!

--- src/components/header/nav/nav.css.js
+++ src/components/header/nav/nav.css.js
@@ -2,17 +2,36 @@ import styled from 'styled-components';
 
 export const Container = styled.nav`
   ul {
-    display: flex;
+    display: block;
     list-style: none;
     padding: 0;
-
+    position: absolute;
+    z-index: 10;
+    top: 20px;
+    background: #fff;
+    right: calc(-25% - 1rem);
+    border: 1px solid #e5e5e5;
     li {
       text-transform: uppercase;
       font-size: 1.3rem;
-
-      & + li {
-        margin-left: 2rem;
-      }
+      padding: 1rem;
+      text-align: center;
     }
   }
+`;
+
+export const Dropdown = styled.div`
+  position: relative;
+  display: inline-block;
+`;
+
+export const Button = styled.div`
+  text-transform: uppercase;
+  cursor: pointer;
+  font-size: 1.3rem;
+`;
+
+export const DropdownList = styled.li`
+  position: relative;
+  display: inline-block;
 `;
--- src/components/header/nav/nav.js
+++ src/components/header/nav/nav.js
@@ -1,15 +1,67 @@
 import React from 'react';
+import { Transition } from 'react-spring';
 import { Link } from 'gatsby';
-import { Container } from './nav.css';
+import { Container, Button, Dropdown, DropdownList } from './nav.css';
 
-const Nav = () => (
-  <Container>
-    <ul>
-      <li>
-        <Link to="/agriculture">Agriculture</Link>
-      </li>
-    </ul>
-  </Container>
-);
+class Nav extends React.Component {
+  constructor() {
+    super();
 
+    this.state = {
+      displayMenu: false,
+    };
+
+    this.showDropdownMenu = this.showDropdownMenu.bind(this);
+    this.hideDropdownMenu = this.hideDropdownMenu.bind(this);
+  }
+
+  showDropdownMenu(event) {
+    event.preventDefault();
+    this.setState({ displayMenu: true }, () => {
+      document.addEventListener('click', this.hideDropdownMenu);
+    });
+  }
+
+  hideDropdownMenu() {
+    this.setState({ displayMenu: false }, () => {
+      document.removeEventListener('click', this.hideDropdownMenu);
+    });
+  }
+
+  render() {
+    const { displayMenu } = this.state;
+    return (
+      <Container>
+        <Dropdown>
+          <Button onClick={this.showDropdownMenu}>Content</Button>
+          <Transition
+            items={displayMenu}
+            from={{ opacity: 0, transform: 'translateY(-10px)' }}
+            enter={{ opacity: 1, transform: 'translateY(0px)' }}
+            leave={{ opacity: 0, transform: 'translateY(-10px)' }}
+          >
+            {displayMenu =>
+              displayMenu &&
+              (props => {
+                return (
+                  <ul style={props}>
+                    <DropdownList>
+                      <Link to="/agriculture">Agriculture</Link>
+                    </DropdownList>
+                    <DropdownList>
+                      <Link to="/agriculture">Agriculture</Link>
+                    </DropdownList>
+                    <DropdownList>
+                      <Link to="/agriculture">Agriculture</Link>
+                    </DropdownList>
+                  </ul>
+                );
+              })
+            }
+          </Transition>
+        </Dropdown>
+      </Container>
+    );
+  }
+}
 export default Nav;
